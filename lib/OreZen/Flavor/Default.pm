01e0861cdcabf1eb79d826a6adaac37759278453
xaicron <xaicron@gmail.com>
2013-04-12 02:09:25 +0900

    added viewport

--- lib/OreZen/Flavor/Default.pm
+++ lib/OreZen/Flavor/Default.pm
@@ -16,6 +16,7 @@ __DATA__
 <title><?= $title ?></title>
 <link rel="stylesheet" href="static/css/style.css" />
 <link rel="stylesheet" href="static/css/pretty.css" />
+<meta name="viewport" content="width=devide-width,initial-scale=0.8,user-scalable=no" />
 </head>
 <body>
 <?= $content ?>
